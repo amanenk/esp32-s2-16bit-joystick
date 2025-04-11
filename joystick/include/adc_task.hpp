5c6a0ca95b3ef261256d1d447063adf865d7f7e1
Andrii Romanenko <andrejjrom@gmail.com>
2025-04-11 16:03:27 +0300

    fix

--- joystick/include/adc_task.hpp
+++ joystick/include/adc_task.hpp
@@ -21,6 +21,9 @@ void adc_task(void *pvParameter) {
   Log.info(F("Native ADC reading task started"));
   analogReadResolution(12);
 
+  int16_t min_values[3] = {4095, 4095, 4095};
+  int16_t max_values[3] = {0, 0, 0};
+
   // create  task that reads adc example on gpio0..gpio3
   int pins[4] = {1, 2, 3};
 
@@ -50,24 +53,33 @@ void adc_task(void *pvParameter) {
       for (int i = 0; i < 3; i++) {
         values[i] = get_avg_from_buffer(buffers[i]);
       }
-      // invert adc_chanels[0] values
-      values[0] = AXIS_MAX_VALUE - values[0];
-      values[1] = AXIS_MAX_VALUE - values[1];
-      values[2] = AXIS_MAX_VALUE - values[2];
+    
+      int16_t new_values[3];
+      for (int i = 0; i < 3; i++) {
+        if (values[i] < min_values[i]) {
+          min_values[i] = values[i];
+        }
+        if (values[i] > max_values[i]) {
+          max_values[i] = values[i];
+        }
+        
+        new_values[i] = map(values[i], min_values[i], max_values[i], 0, AXIS_MAX_VALUE);
+      }
 
-      // map values to 0..3000
-      values[0] = map(values[0], 500, 2600, 0, AXIS_MAX_VALUE);
-      values[1] = map(values[1], 500, 2600, 0, AXIS_MAX_VALUE);
-      values[2] = map(values[2], 500, 2600, 0, AXIS_MAX_VALUE);
-
-      gamepad.joystick1(values[0], values[1], values[2]);
+      gamepad.joystick1(new_values[0], new_values[1], new_values[2]);
       // gamepad.joystick1(0,0,0);
       period = millis() - start_time;
       // print period every second
       static unsigned long last_time = 0;
       if (millis() - last_time > 1000) {
-        Log.info("adc native period: %d, %d\t%d\t%d\n", period, values[0],
+        Log.info("read_values: %d\t%d\t%d\n", values[0],
                       values[1], values[2]);
+        Log.trace("min values: %d\t%d\t%d\n", min_values[0], min_values[1],
+                  min_values[2]);
+        Log.trace("max values: %d\t%d\t%d\n", max_values[0], max_values[1],
+                  max_values[2]);
+        Log.trace("transformed values: %d\t%d\t%d\n", new_values[0],
+                  new_values[1], new_values[2]);
         last_time = millis();
       }
       delay(5);
